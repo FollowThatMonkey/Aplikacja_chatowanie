09f285e4b606e7300872d25aff974a1437491a03
Rafaello <rafjar@Rafalek>
2020-07-16 13:47:57 +0200

    Rewriting whole app

--- serwer/server.py
+++ serwer/server.py
@@ -1,177 +1,220 @@
-# local imports
-import user
-# std imports
+import sqlite3
+import socket
+import re
+import os
 import threading
 import logging
-import socket
-import sqlite3
-import os
-import sys
-import re
+import traceback
 
-ERROR_CODE = 1
-DATABASE_LOC = os.path.dirname(os.path.realpath(__file__)) + '/users.db'
+DB_PATH = os.path.dirname(os.path.abspath(__file__)) + '/users.db'
 BUFF_SIZE = 1024
 ENCODING = 'utf-8'
 
+DB_ERROR = 1
+SOCKET_ERROR = 2
+KEYBOARD_ERROR = 3
+
+
+class Message:
+    def __init__(self):
+        pass
+
+
+class Client:
+    def __init__(self, client_sock: socket.socket, username: str):
+        pass
+
 
 class Server:
-    LOGIN_REGEX = re.compile(r'LOGIN\s+(\w+)\s+(\w+)')
-    REGISTER_REGEX = re.compile(r'REGISTER\s+(\w+)\s+(\w+)')
-    HELP_REGEX = re.compile(r'HELP')
-    EXIT_REGEX = re.compile(r'EXIT')
+    REGISTER_REGEX = re.compile(r'REGISTER\s+(\w+)\s+(\S+)\s*')
+    LOGIN_REGEX = re.compile(r'LOGIN\s+(\w+)\s+(\S+)\s*')
+    HELP_REGEX = re.compile(r'HELP\s*')
+    EXIT_REGEX = re.compile(r'EXIT\s*')
 
-    def __init__(self, PORT: int, nCONNECTIONS: int):
+    def __init__(self, PORT: int, nConnections: int):
         self.PORT = PORT
-        self.nCONNECTIONS = nCONNECTIONS
+        self.nConnections = nConnections
 
         self.logging_init()
-        self.database_init()
-        self.serv_sock = self.socket_init()
+        self.db_init()
+        self.server_socket = self.socket_init()
 
         try:
-            self.accept_connections()
+            self.accept_conn()
 
         except KeyboardInterrupt:
-            logging.error('Keyboard Interrupt detected...')
-            sys.exit(ERROR_CODE)
+            logging.error('Keyboard interrupt detected...\nShutting down...')
+            self.close_server()
 
         finally:
-            self.close_server()
+            os.sys.exit(KEYBOARD_ERROR)
+
+    def close_server(self, ERROR_CODE=None):
+        logging.info('Closing socket...')
+        self.server_socket.close()
 
     def logging_init(self) -> None:
         logging.basicConfig(format='[{asctime}] {levelname} - {message}',
                             datefmt='%d/%m/%Y %H:%M:%S', style='{', level=0)
 
-    def database_init(self) -> None:
+    def db_init(self) -> None:
         try:
-            with sqlite3.connect(DATABASE_LOC) as db_conn:
-                db_cursor = db_conn.cursor()
-                db_cursor.execute(
-                    '''CREATE TABLE IF NOT EXISTS User(
-                        IDuser INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                        username TEXT TYPE UNIQUE NOT NULL,
+            logging.info('Initializing database...')
+            with sqlite3.connect(DB_PATH) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    """ CREATE TABLE IF NOT EXISTS users(
+                        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT NOT NULL,
                         password TEXT NOT NULL,
-                        status INTEGER
-                        );'''
+                        CONSTRAINT username_constraint UNIQUE (username)
+                    ); """
                 )
-                db_cursor.execute(
-                    '''CREATE TABLE IF NOT EXISTS Friend(
-                        IDuser INTEGER,
-                        IDfriend INTEGER,
-                        FOREIGN KEY(IDuser, IDfriend) REFERENCES User(IDuser, IDuser)
-                    );'''
+                cursor.execute(
+                    """ CREATE TABLE IF NOT EXISTS firends(
+                        user1 INTEGER,
+                        user2 INTEGER,
+                        CONSTRAINT fk_column
+                            FOREIGN KEY (user1, user2)
+                            REFERENCES users (user_id, user_id)
+                    ); """
                 )
-                db_cursor.execute(
-                    '''CREATE TABLE IF NOT EXISTS Message(
-                        IDmessage INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                        message TEXT,
-                        sender INTEGER,
+                cursor.execute(
+                    """ CREATE TABLE IF NOT EXISTS messages(
+                        message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        body TEXT,
                         addressee INTEGER,
-                        FOREIGN KEY(sender, addressee) REFERENCES User(IDuser, IDuser)
-                    );'''
+                        CONSTRAINT fk_column
+                            FOREIGN KEY (addressee)
+                            REFERENCES users (user_id)
+                    ); """
                 )
-                db_conn.commit()
-                logging.info('Database loaded correctly...')
+                conn.commit()
+            logging.info('Database initialized...')
 
         except Exception as e:
-            logging.error(f'Database error {e}. Shutting down...')
-            sys.exit(ERROR_CODE)
+            traceback.print_exc()
+            logging.error(
+                f'Database initialization error: {e}\nShutting down...')
+            os.sys.exit(DB_ERROR)
 
-    def socket_init(self):
+    def socket_init(self) -> socket.socket:
         try:
-            serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            serv_sock.bind(('localhost', self.PORT))
-            serv_sock.listen(self.nCONNECTIONS)
-            logging.info('Socket set correctly...')
+            logging.info('Initializing server socket...')
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_socket.bind(('localhost', self.PORT))
+            server_socket.listen(self.nConnections)
+
+            logging.info('Server socket initialized...')
+            return server_socket
 
         except Exception as e:
-            logging.error('Error while setting socket. Shutting down...')
-            logging.error(f'{e}')
-            sys.exit(ERROR_CODE)
+            traceback.print_exc()
+            logging.error(
+                f'Error while initializing server socket...\nShutting down...')
+            os.sys.exit(SOCKET_ERROR)
 
-        return serv_sock
-
-    def accept_connections(self) -> None:
+    def accept_conn(self) -> None:
         while True:
-            client_socket, client_address = self.serv_sock.accept()
+            client_sock, client_addr = self.server_socket.accept()
 
-            logging.info(f'{client_address} has connected...')
-            th = threading.Thread(
-                target=self.handle_connection, args=(client_socket,))
+            logging.info(f"{client_addr} has connected...")
+            th = threading.Thread(target=self.handle_conn, args=(client_sock,))
             th.start()
 
-    def close_server(self) -> None:
-        logging.info('Shutting down...')
-        self.serv_sock.close()
-
-    def handle_connection(self, client_socket: socket.socket) -> None:
+    def handle_conn(self, client_sock: socket.socket) -> None:
         try:
-            client = self.login_user(client_socket)
+            client = self.client_init(client_sock)
+
             if client:
-                th_send = threading.Thread(target=client.send_msg)
-                th_recv = threading.Thread(target=client.recv_msg)
+                th_send = threading.Thread()
+                th_recv = threading.Thread()
                 th_send.start()
                 th_recv.start()
                 th_send.join()
                 th_recv.join()
 
         finally:
-            logging.info(f'{client_socket.getsockname()} has disconnected...')
-            client_socket.close()
+            client_sock.close()
 
-    def login_user(self, client_socket: socket.socket) -> user.User:
-        welcome_msg = ("Welcome to server!\n"
-                       "If you want to log in type 'LOGIN USERNAME PASSWORD', or if you want to register in type 'REGISTER USERNAME PASSWORD'.\n"
-                       "Anytime you need help, type 'HELP'.\n"
-                       ).encode(ENCODING)
-        client_socket.sendall(welcome_msg)
+    def client_init(self, client_sock: socket.socket) -> Client:
+        greeting_msg = ("Welcome to the server!\n"
+                        "Register by typing 'REGISTER username password' or log in by typing 'LOGIN username password'.\n"
+                        "Antype you need help, just type 'HELP' :)\n").encode(ENCODING)
+        client_sock.sendall(greeting_msg)
 
-        finish_loop = False
-        while False:
-            msg = client_socket.recv(BUFF_SIZE).decode(ENCODING)
-
-            # case login
-            match = re.fullmatch(self.LOGIN_REGEX, msg)
-            if match:
-                username, password = match.groups()
-                client = self.login_client(username, password)
-
-                if client:
-                    return client
+        while True:
+            msg = client_sock.recv(BUFF_SIZE).decode(ENCODING)
 
             # case register
-            match = re.fullmatch(self.REGISTER_REGEX, msg)
+            match = self.REGISTER_REGEX.fullmatch(msg)
             if match:
                 username, password = match.groups()
-                client = self.register_client(username, password)
+                client = self.register_client(client_sock, username, password)
 
                 if client:
                     return client
+                else:
+                    continue
+
+            # case login
+            match = self.LOGIN_REGEX.fullmatch(msg)
+            if match:
+                username, password = match.groups()
+                client = self.login_client(client_sock, username, password)
+
+                if client:
+                    return client
+                else:
+                    continue
 
             # case help
-            match = re.fullmatch(self.HELP_REGEX, msg)
+            match = self.HELP_REGEX.fullmatch(msg)
             if match:
-                client_socket.sendall(HELP_MSG)
+                self.send_help(client_sock)
+                continue
 
             # case exit
-            match = re.fullmatch(self.EXIT_REGEX)
+            match = self.EXIT_REGEX.fullmatch(msg)
             if match:
+                self.exit_client(client_sock)
                 return None
 
-        def login_client(self, username: str, password: str) -> user.User:
-            with sqlite3.connect(DATABASE_LOC) as conn:
+            msg = "Unknown command. Try again!\nType 'HELP' to show avaiable commands!\n".encode(
+                ENCODING)
+            client_sock.sendall(msg)
+
+    def register_client(self, client_sock: socket.socket, username: str, password: str) -> Client:
+        try:
+            with sqlite3.connect(DB_PATH) as conn:
                 cursor = conn.cursor()
                 cursor.execute(
-                    """ SELECT username, password FROM User WHERE username=?; """,
-                    (username,)
+                    """ INSERT INTO users(username, password) VALUES (
+                        ?, ?
+                    ); """, (username, password)
                 )
-                user_search = cursor.fetchall()
+                conn.commit()
+                logging.info(f'Registered {username}')
+                return None
 
-        def register_client(self, username: str, password: str) -> user.User:
-            pass
+        except sqlite3.IntegrityError as e:
+            traceback.print_exc()
+            logging.error(f"Error while registering new client: {e}")
+            return None
+
+    def login_client(self, client_sock: socket.socket, username: str, password: str) -> Client:
+        pass
+
+    def send_help(self, client_sock: socket.socket) -> None:
+        pass
+
+    def exit_client(self, client_sock: socket.socket) -> None:
+        exit_mgs = "You're being disconnected from server...\n".encode(
+            ENCODING)
+        client_sock.sendall(exit_mgs)
+        client_sock.close()
 
 
 if __name__ == '__main__':
-    Server(40123, 1)
+    Server(40123, 10)
